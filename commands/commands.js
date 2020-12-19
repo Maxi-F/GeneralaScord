763eeacc03e8aac4e4cc095cb3ad0c2581e606fc
Mateo Feldman <feldman.mateo@gmail.com>
2020-12-19 16:09:16 -0300

    Added more dice logic (now with objects)

--- commands/commands.js
+++ commands/commands.js
@@ -3,21 +3,49 @@ const { sendMessageTo } = require('../utils/messages');
 
 const notFound = (command, message) => sendMessageTo(message.channel.id, `${command} is not a command.`)
 
-const options = (result)     => {
-    let magia = ["Unos", "Doses", "Treses", "Cuatros", "Cincos", "Seises"];
-    let opts = [];
-    result.forEach(element => {
-        const aux = magia[element - 1];
-        if(!opts.includes(aux)) {
-            opts.push(aux);
-        } 
+const addNumericDiceOptions = (result, opts) => {
+    let numericOptions = ["unos", "doses", "treses", "cuatros", "cincos", "seises"];
+    result.forEach(number => {
+        const numericActualOption = numericOptions[number - 1];
+        const foundOption = opts.find((option) => option.opt === numericActualOption) 
+        if(!foundOption) {
+            opts.push({opt: numericActualOption, count: 1});
+        } else {
+            foundOption.count++
+        }
     });
+}
+
+const addStraight = (result, opts) => {
+    if (opts.length >= 5 && ![1,2,6].every((num) => result.includes(num))) opts.push("Escalera"); // banana
+}
+
+const addDouble = (result, opts) => {
+
+}
+
+const options = (result)     => {
+    let numericOpts = [];
+    let opts;
+    
+    // Agrega opciones numericas (1,2,3,...,6)
+    addNumericDiceOptions(result, numericOpts);
+
+    opts = [...numericOpts].map(number => number.opt);  
+
+    // Agrega la opcion de Escalera
+    addStraight(result, opts);
+    
+    // Agrega opcion de doble
+    addDouble(opts, numericOpts);
+
+
+
     return opts;
 }
 
 const rollDice = (message)   => {
-    let result = roll(6).sort();
-
+    let result = roll(5).sort();
     return sendMessageTo(message.channel.id, `rolled dice: ${result}\noptions: ${options(result)}`)
 }
 
