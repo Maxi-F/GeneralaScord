be653ff17591ae8d0a98d21b1a9cdf99eca63f4d
Maxi-F <maxifeldman@hotmail.com>
2020-12-19 20:41:00 -0300

    created embed for message

--- utils/messages.js
+++ utils/messages.js
@@ -1,8 +1,14 @@
 const bot = require('../bot.js')
 
+const createEmbed = (message) => ({
+  color: '#0099ff',
+  title: 'GeneralaScord',
+  description: message
+})
+
 const sendMessageTo = (channelId, message) => {
   const channel = bot.channels.cache.get(channelId);
-  channel.send(message)
+  channel.send({ embed: createEmbed(message) })
 }
 
 
