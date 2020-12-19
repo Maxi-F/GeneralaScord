d6dc976f19e9500aee8af25f3e816f6ce898b430
Maxi-F <maxifeldman@hotmail.com>
2020-12-19 20:43:19 -0300

    added optional embedInfo

--- utils/messages.js
+++ utils/messages.js
@@ -1,14 +1,15 @@
 const bot = require('../bot.js')
 
-const createEmbed = (message) => ({
+const createEmbed = (message, embedInfo) => ({
   color: '#0099ff',
   title: 'GeneralaScord',
-  description: message
+  description: message,
+  ...embedInfo
 })
 
-const sendMessageTo = (channelId, message) => {
+const sendMessageTo = (channelId, message, embedInfo = {}) => {
   const channel = bot.channels.cache.get(channelId);
-  channel.send({ embed: createEmbed(message) })
+  channel.send({ embed: createEmbed(message, embedInfo) })
 }
 
 
